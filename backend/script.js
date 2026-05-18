fdfc7e0f0daf6817daf782043b485d05ca6858e6
Iblamedawe <puskasdavid2008@gmail.com>
2026-05-18 08:34:52 +0200

    sql_osszekotes_base

---
+++ backend/script.js
@@ -0,0 +1,32 @@
+const mysql = require('mysql2');
+
+// kapcsolat létrehozása
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'root',
+  database: 'test'
+});
+
+// csatlakozás
+connection.connect(err => {
+  if (err) {
+    console.error('Hiba:', err);
+    return;
+  }
+  console.log('Csatlakozva a MySQL-hez!');
+});
+
+// lekérdezés példa
+connection.query('SELECT * FROM felhasznalo', (err, results) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(results);
+});
+
+
+
+// kapcsolat lezárása
+connection.end();
